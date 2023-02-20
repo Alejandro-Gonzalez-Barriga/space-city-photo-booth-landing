b091b2549328021cd6fed7309c8af31659b11b88
Alejandro <alejandro.gonzalez.barriga@gmail.com>
2023-02-19 19:44:06 -0600

    installed nodemon

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          SpaceCityLanding
+          🔥 SpaceCityLanding 🔥
         </p>
         <a
           className="App-link"
