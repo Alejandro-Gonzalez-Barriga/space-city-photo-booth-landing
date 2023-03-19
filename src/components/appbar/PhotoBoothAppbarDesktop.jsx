d82df6679c0d379152b584ac9bb37954c76225bc
Alejandro <alejandro.gonzalez.barriga@gmail.com>
2023-03-19 07:42:19 -0600

    added jsx

--- src/components/appbar/PhotoBoothAppbarDesktop.jsx
+++ src/components/appbar/PhotoBoothAppbarDesktop.jsx
@@ -1,8 +1,11 @@
 import { AppbarContainer, AppbarHeader } from "../../styles/appbar";
+import logo from '../../spaceCityPhotoBoothLogoIcon.svg';
+
 
 export default function PhotoBoothAppbarDesktop({ matches}) {
     return (
-       <AppbarContainer>
+       <AppbarContainer> 
+        <img src={logo} className="App-logo2" alt="logo" />
         <AppbarHeader>Space City Photo Booth</AppbarHeader>
        </AppbarContainer>
     );
