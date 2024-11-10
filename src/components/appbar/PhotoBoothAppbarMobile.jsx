25eec2df49a9a0c7dac2adf8e791172e51c5525d
Alejandro Gonzalez Barriga <alejandro.gonzalez.barriga@gmail.com>
2024-11-10 17:16:36 -0600

    compiling

--- src/components/appbar/PhotoBoothAppbarMobile.jsx
+++ src/components/appbar/PhotoBoothAppbarMobile.jsx
@@ -1,18 +1,19 @@
 import { IconButton } from "@mui/material";
-import { Menu as MenuIcon } from "@material-ui/icons";
+import { Menu as MenuIcon } from "@mui/icons-material"; // Updated import
 import { AppbarContainer, AppbarHeader } from "../../styles/appbar";
 import { Colors } from "../../styles/theme";
 import logo from '../../spaceCityPhotoBoothLogoIcon.svg';
 
-
-export default function PhotoBoothAppbarMobile({ matches, hasScrolled}) {
+export default function PhotoBoothAppbarMobile({ matches, hasScrolled }) {
     return (
-       <AppbarContainer  sx={{background: hasScrolled ? "rgba(0,0,0,0.6)" : "transparent"}}>
-        <img src={logo} className="App-logo2" alt="logo" />
-        <AppbarHeader  sx={{color: hasScrolled ? Colors.white : Colors.secondary}}>Photo Booth</AppbarHeader>
-            <IconButton  sx={{color: hasScrolled ? Colors.white : Colors.secondary}}>
-                <MenuIcon/>
-            </IconButton>
+       <AppbarContainer sx={{ background: hasScrolled ? "rgba(0,0,0,0.6)" : "transparent" }}>
+          <img src={logo} className="App-logo2" alt="logo" />
+          <AppbarHeader sx={{ color: hasScrolled ? Colors.white : Colors.secondary }}>
+              Photo Booth
+          </AppbarHeader>
+          <IconButton sx={{ color: hasScrolled ? Colors.white : Colors.secondary }}>
+              <MenuIcon />
+          </IconButton>
        </AppbarContainer>
     );
 }
